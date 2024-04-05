d9015f6397906373d96751f9d1a8575255f40edb
Shawn <tomatosale7774@gmail.com>
2024-04-05 11:37:51 +0900

    Initial Commit

---
+++ LadybugPoker/LadybugPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LadybugPokerApp.swift
+//  LadybugPoker
+//
+//  Created by 박진서 on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LadybugPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
