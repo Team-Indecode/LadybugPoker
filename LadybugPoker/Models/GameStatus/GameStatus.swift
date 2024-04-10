fd49d0627966bc14cba2843c62da65226b241089
Shawn <tomatosale7774@gmail.com>
2024-04-10 17:07:05 +0900

    Update GameStatus.swift
    설명을 추가했습니다.

--- LadybugPoker/Models/GameStatus/GameStatus.swift
+++ LadybugPoker/Models/GameStatus/GameStatus.swift
@@ -8,8 +8,15 @@
 import Foundation
 
 enum GameStatus {
+    // 플레이어가 부족한 상태 1~2명
     case notEnoughUsers
+    
+    // 플레이어가 충분하지만 시작하지 않은 상태
     case notStarted
+    
+    // 게임 중
     case onAir
+    
+    // 게임 종료
     case finished
 }
