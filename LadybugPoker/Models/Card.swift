bc4719d2bb7775c24495e05c493bca883da9f8b9
Shawn <tomatosale7774@gmail.com>
2024-04-15 14:29:14 +0900

    [feat] Card -> Identifiable

--- LadybugPoker/Models/Card.swift
+++ LadybugPoker/Models/Card.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-struct Card: Codable, Hashable {
+struct Card: Codable, Hashable, Identifiable {
+    public var id: String {
+        return self.bug.rawValue
+    }
     /// 어떤 타입의 벌레인지
     let bug: Bugs
     /// 해당 카드가 몇장이 있는지
