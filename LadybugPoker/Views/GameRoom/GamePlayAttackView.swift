6efb6a2b10c993d32d752be69bdc1a5da12cf58e
rayoung <rlafkdud122i@gmail.com>
2024-04-19 00:47:52 +0900

    play attack view edit

--- LadybugPoker/Views/GameRoom/GamePlayAttackView.swift
+++ LadybugPoker/Views/GameRoom/GamePlayAttackView.swift
@@ -11,12 +11,21 @@ import NukeUI
 /// 공격자 입장
 struct GamePlayAttackView: View {
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
-    let bugs: [Bugs] = [.frog, .ladybug, .frog, .rat, .spider, .snail, .worm, .bee]
+    let bugs: [Bugs] = [.snake, .ladybug, .frog, .rat, .spider, .snail, .worm, .bee]
     var body: some View {
         VStack {
-            makeUserView(User(id: "", displayName: "라영", profileUrl: "https://picsum.photos/200"))
+            HStack {
+                makeUserView(User(id: "", displayName: "라영", profileUrl: "https://picsum.photos/200"))
+                Spacer()
+            }
+            .background(Color.red)
+            Spacer()
             bugsView
-            makeUserView(User(id: "", displayName: "fkdud", profileUrl: "https://picsum.photos/200"))
+            Spacer()
+            HStack {
+                Spacer()
+                makeUserView(User(id: "", displayName: "fkdud", profileUrl: "https://picsum.photos/200"))
+            }
         }
     }
     
@@ -26,13 +35,15 @@ struct GamePlayAttackView: View {
         return HStack {
             if let profileUrl = user.profileUrl {
                 LazyImage(url: URL(string: profileUrl))
+                    .scaledToFit()
                     .frame(width: 50, height: 50)
                     .clipShape(Circle())
+                    .padding(.leading, 10)
             }
             Text(user.displayName)
                 .font(.sea(20))
         }
-        .frame(width: 171, height: 69)
+        .frame(width: 171, height: 69, alignment: .leading)
         .background(Color.white)
         .clipShape(Capsule())
     }
@@ -47,7 +58,10 @@ struct GamePlayAttackView: View {
     
     func makeBug(_ bug: Bugs) -> some View {
         Image(bug.id)
+            .resizable()
+            .padding(6)
             .frame(width: 70, height: 70)
+            .scaledToFit()
             .background(Color(hex: "D9D9D9"))
             .clipShape(Circle())
     }
