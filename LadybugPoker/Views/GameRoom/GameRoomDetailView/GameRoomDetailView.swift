0b578b4d55e724ee25d75eec6a9aae42ded2426f
Shawn <tomatosale7774@gmail.com>
2024-04-22 14:32:42 +0900

    [feat] GameRoomDetailView

---
+++ LadybugPoker/Views/GameRoom/GameRoomDetailView/GameRoomDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  GameRoomDetailView.swift
+//  LadybugPoker
+//
+//  Created by 박진서 on 4/22/24.
+//
+
+import SwiftUI
+
+struct GameRoomDetailView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    GameRoomDetailView()
+}
