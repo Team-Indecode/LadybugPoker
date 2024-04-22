75270e11ee5ecc0ddceb3a18369031f6f487c0c1
Shawn <tomatosale7774@gmail.com>
2024-04-22 16:53:06 +0900

    [feat] MainView NavStack 제거

--- LadybugPoker/Views/Main/MainView.swift
+++ LadybugPoker/Views/Main/MainView.swift
@@ -12,33 +12,31 @@ struct MainView: View {
     @State private var gameRooms: [GameRoom] = []
     
     var body: some View {
-        NavigationStack(path: $service.path) {
-            VStack {
-                ForEach(gameRooms) { gameRoom in
-                    GameRoomView(gameRoom: gameRoom)
-                }
-                
-                Spacer()
-                
-                
-                Button {
-                    service.path.append(.createGameRoom)
-                } label: {
-                    Color.bugDark
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay {
-                            Text("방 만들기")
-                                .font(.sea(20))
-                                .foregroundStyle(Color.white)
-                        }
-                }
-                .frame(height: 50)
-                .padding(.horizontal, 20)
+        VStack {
+            ForEach(gameRooms) { gameRoom in
+                GameRoomView(gameRoom: gameRoom)
             }
-            .background {
-                Color.bugLight
-                    .ignoresSafeArea()
+            
+            Spacer()
+            
+            
+            Button {
+                service.path.append(.createGameRoom)
+            } label: {
+                Color.bugDark
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay {
+                        Text("방 만들기")
+                            .font(.sea(20))
+                            .foregroundStyle(Color.white)
+                    }
             }
+            .frame(height: 50)
+            .padding(.horizontal, 20)
+        }
+        .background {
+            Color.bugLight
+                .ignoresSafeArea()
         }
         .onAppear {
             gameRooms = GameRoom.listPreview
