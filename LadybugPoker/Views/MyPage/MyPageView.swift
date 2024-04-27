b4670ef6e7bfaceb0f6d848905c036df82eab873
Shawn <tomatosale7774@gmail.com>
2024-04-27 14:04:46 +0900

    Update MyPageView.swift

--- LadybugPoker/Views/MyPage/MyPageView.swift
+++ LadybugPoker/Views/MyPage/MyPageView.swift
@@ -8,25 +8,35 @@
 import SwiftUI
 
 struct MyPageView: View {
+    @EnvironmentObject private var service: Service
+    
     let id: String
     
     var body: some View {
         VStack {
             HStack {
-                Spacer()
-                
-                Image("ladybug")
-                    .resizable()
-                    .frame(width: 27, height: 27)
-                
-                Text("무당벌레 포커")
-                    .font(.sea(15))
+                Button {
+                    service.path.removeLast()
+                } label: {
+                    Image(systemName: "chevron.left")
+                }
+                .foregroundStyle(Color.black)
                 
                 Spacer()
             }
             .padding(.horizontal, 15)
             .padding(.vertical, 20)
             .background(Color.bugDarkMedium)
+            .overlay {
+                HStack {
+                    Image("ladybug")
+                        .resizable()
+                        .frame(width: 27, height: 27)
+                    
+                    Text("무당벌레 포커")
+                        .font(.sea(15))
+                }
+            }
             
             Spacer()
         }
