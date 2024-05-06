d55a161521ee04442da806181afa5f127f28bac4
Shawn <tomatosale7774@gmail.com>
2024-05-06 18:31:05 +0900

    [feat] Random players

--- LadybugPoker/Views/GuideView/GuideView.swift
+++ LadybugPoker/Views/GuideView/GuideView.swift
@@ -8,14 +8,106 @@
 import SwiftUI
 
 struct GuideView: View {
+    @EnvironmentObject private var service: Service
+    
+    @State private var users: [User] = []
+    
     var body: some View {
         VStack {
+            if users.count == 6 {
+                HStack {
+                    GuideView_PlayerBoardView(isLeft: true, user: users[0])
+                    
+                    Spacer()
+                    
+                    GuideView_PlayerBoardView(isLeft: true, user: users[1])
+                }
+                .padding(.horizontal, 10)
+                
+                HStack {
+                    GuideView_PlayerBoardView(isLeft: true, user: users[2])
+                    
+                    Spacer()
+                    
+                    GuideView_PlayerBoardView(isLeft: true, user: users[3])
+                }
+                .padding(.horizontal, 10)
+                
+                HStack {
+                    GuideView_PlayerBoardView(isLeft: true, user: users[4])
+                    
+                    Spacer()
+                    
+                    GuideView_PlayerBoardView(isLeft: true, user: users[5])
+                }
+                .padding(.horizontal, 10)
+            }
             
+            Spacer()
+            
+            Color(hex: "d9d9d9")
+                .frame(height: 2)
+            
+            Text("게임시작 전 입니다.")
+                .font(.sea(15))
+            
+            
+            Text("준비 완료")
+                .font(.sea(35))
+                .padding(.vertical, 40)
+            
+            Text("준비 취소")
+                .font(.sea(15))
+        }
+        .background(Color.bugLight)
+        .overlay {
+            ZStack {
+                Color.black
+                    .opacity(0.8)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Image("ladybug")
+                        .padding(.bottom, 30)
+                    
+                    Text("무당벌레 포커에 오신 것을")
+                    
+                    Text("환영합니다 !")
+                }
+                .font(.sea(30))
+                .foregroundStyle(Color.white)
+            }
+            .overlay(alignment: .bottom) {
+                Button {
+                    
+                } label: {
+                    Color.black
+                        .frame(height: 38)
+                }
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding(.horizontal, 35)
+                .padding()
+            }
+        }
+        .onAppear {
+            /// Test Code
+            service.myUserModel = User.random()
+            
+            if let user = service.myUserModel {
+                users.insert(user, at: 0)
+            }
+            
+            for _ in 0..<5 {
+                users.append(User.random())
+            }
         }
     }
 }
 
 #Preview {
-    GuideView()
+    VStack {
+        GuideView()
 
+    }
+    .environmentObject(Service.shared)
 }
--- LadybugPoker/Views/GuideView/SubViews/GuideView+PlayerBoardView.swift
+++ LadybugPoker/Views/GuideView/SubViews/GuideView+PlayerBoardView.swift
@@ -6,15 +6,41 @@
 //
 
 import SwiftUI
+import NukeUI
 
-struct GuideView_PlayerBoardView: View {
-    var body: some View {
-        VStack {
-            
+extension GuideView {
+    struct GuideView_PlayerBoardView: View {
+        @State var isLeft: Bool
+        @State var user: User
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                HStack(spacing: 8) {
+                    LazyImage(url: URL(string: user.profileUrl ?? ""))
+                        .scaledToFill()
+                        .frame(width: 30, height: 30)
+                        .clipShape(Circle())
+                    
+                    VStack(alignment: .leading) {
+                        Text(user.displayName)
+                        
+                        HStack(spacing: 3) {
+                            Image("profileCardIcon")
+
+                            Text("\(Int.random(in: 5..<10))장")
+                        }
+                    }
+                    .font(.sea(8))
+
+                }
+                
+                Text("준비 완료")
+                    .font(.sea(35))
+            }
         }
     }
 }
 
 #Preview {
-    GuideView_PlayerBoardView()
+    GuideView.GuideView_PlayerBoardView(isLeft: true, user: User.random())
 }
