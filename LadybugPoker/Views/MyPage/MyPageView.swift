25c07cfc58594ca284f58110f3e41f7e9cf73490
Shawn <tomatosale7774@gmail.com>
2024-06-10 15:44:08 +0900

    Update MyPageView.swift

--- LadybugPoker/Views/MyPage/MyPageView.swift
+++ LadybugPoker/Views/MyPage/MyPageView.swift
@@ -74,50 +74,52 @@ struct MyPageView: View {
             }
             .padding(.horizontal, 15)
             
-            HStack {
-                Text("게임 히스토리")
-                    .font(.sea(18))
+            ScrollView {
+                HStack {
+                    Text("게임 히스토리")
+                        .font(.sea(18))
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 25)
+                .padding(.top, 50)
                 
-                Spacer()
-            }
-            .padding(.horizontal, 25)
-            .padding(.top, 50)
-            
-            LazyVStack {
-                ForEach(histories) { history in
-                    VStack {
-                        HStack {
-                            Text(history.title)
+                LazyVStack {
+                    ForEach(histories) { history in
+                        VStack {
+                            HStack {
+                                Text(history.title)
 
+                                Spacer()
+                                
+                                Text("\(history.userCount) / \(history.maxUserCount)")
+                            }
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 15)
+                            
                             Spacer()
-                            
-                            Text("\(history.userCount) / \(history.maxUserCount)")
                         }
+                        .font(.sea(15))
+                        .frame(height: 100)
+                        .overlay {
+                            Text(history.isWinner ? "WIN" : "LOSE")
+                                .font(.sea(50))
+                        }
+                        .background {
+                            ZStack {
+                                Color.bugLight
+                                
+                                Color.black.opacity(0.3)
+                            }
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
                         .padding(.horizontal, 20)
-                        .padding(.vertical, 15)
-                        
-                        Spacer()
+                        .padding(.vertical, 7)
                     }
-                    .font(.sea(15))
-                    .frame(height: 100)
-                    .overlay {
-                        Text(history.isWinner ? "WIN" : "LOSE")
-                            .font(.sea(50))
-                    }
-                    .background {
-                        ZStack {
-                            Color.bugLight
-                            
-                            Color.black.opacity(0.3)
-                        }
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 7)
                 }
             }
+
             
-            Spacer()
         }
         .background(Color.bugLight)
         .onAppear {
